6c153cc9458fa1609edb1070f43965f00e747857
Koushik Dey <koushik.dey@live.com>
2023-03-22 01:05:57 +0200

    s

--- posts/index.js
+++ posts/index.js
@@ -2,10 +2,10 @@ const express = require ('express');
 
 const app = express();
 
-app.get('/posts', (req, res) => {
+app.get('/posts', (req, res) => {});
 
-});
+app.post('/posts', (req, res) => {});
 
-app.post('/posts', (req, res) => {
-
-});
+app.listen(4000, () => {
+    console.log('Listening on 4000');
+})
