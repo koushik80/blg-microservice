da63fc546106516c1752bc7fa1a56156e0c4ed43
Koushik Dey <koushik.dey@live.com>
2023-03-26 14:29:05 +0300

    receiving events

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,13 +8,13 @@ app.use(bodyParser.json());
 app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post('https://localhost:4000/events', event).catch((err) => {
+  axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('https://localhost:4001/events', event).catch((err) => {
+  axios.post('http://localhost:4001/events', event).catch((err) => {
     console.log(err.message);
   });
-  axios.post('https://localhost:4002/events', event).catch((err) => {
+  axios.post('http://localhost:4002/events', event).catch((err) => {
     console.log(err.message);
   });
   res.send({ status: 'OK' });
--- posts/index.js
+++ posts/index.js
@@ -26,11 +26,18 @@ app.post('/posts', async (req, res) => {
   await axios.post('http://localhost:4005/events', {
     type: 'PostCreated',
     data: {
-      id, title
+      id,
+      title
     }
   });
 
   res.status(200).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
